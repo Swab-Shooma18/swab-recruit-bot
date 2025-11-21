8ebeea0b51f1f49d818e334a2f6f451cbb53d4a7
Ruben Van Hecke <r0808423@student.thomasmore.be>
2025-11-21 19:33:54 +0100

    jadandskotizo met with snark

--- utils/getJadAndSkotizo.js
+++ utils/getJadAndSkotizo.js
@@ -5,15 +5,19 @@ export async function getJadAndSkotizo(username) {
     const url = `https://roatpkz.com/hiscore/user/${encodeURIComponent(username)}/normal/`;
     const res = await fetch(url);
     const html = await res.text();
+    const $ = cheerio.load(html);
 
-    const jadRegex = /TzTok-Jad Kills[\s\S]*?<td class="value">\s*([\d,]+)\s*<\/td>/i;
-    const skotizoRegex = /Skotizo Kills[\s\S]*?<td class="value">\s*([\d,]+)\s*<\/td>/i;
+    const values = $("td.value");
 
-    const jadMatch = html.match(jadRegex);
-    const skotizoMatch = html.match(skotizoRegex);
+    if (values.length < 45) {
+        return { jad: 0, skotizo: 0 };
+    }
+
+    const jad = $(values[72]).text().replace(/,/g, "").trim();
+    const skotizo = $(values[74]).text().replace(/,/g, "").trim();
 
     return {
-        jad: jadMatch ? jadMatch[1].replace(/,/g, "") : 0,
-        skotizo: skotizoMatch ? skotizoMatch[1].replace(/,/g, "") : 0
+        jad: jad || 0,
+        skotizo: skotizo || 0
     };
 }
