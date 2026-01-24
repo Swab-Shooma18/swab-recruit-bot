718b4ca6043906f163875196cc1b8ecc34abbdf3
Ruben Van Hecke <r0808423@student.thomasmore.be>
2026-01-24 18:05:38 +0100

    work on voice activity tracker

--- utils.js
+++ utils.js
@@ -50,7 +50,3 @@ export function getRandomEmoji() {
   const emojiList = ['😭','😄','😌','🤓','😎','😤','🤖','😶‍🌫️','🌏','📸','💿','👋','🌊','✨'];
   return emojiList[Math.floor(Math.random() * emojiList.length)];
 }
-
-export function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
